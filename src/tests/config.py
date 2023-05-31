480294a03f583dc3302959bcc7f43646bc7d6f25
Hans Schabert <schabert@amazon.com>
2023-05-31 17:24:34 +0200

    add env variable as second alternative to samconfig.toml

--- src/tests/config.py
+++ src/tests/config.py
@@ -1,7 +1,9 @@
 import toml
+import os
 import boto3
-sfn = boto3.client('stepfunctions')
-cloudformation = boto3.resource('cloudformation')
+
+sfn = boto3.client("stepfunctions")
+cloudformation = boto3.resource("cloudformation")
 
 
 class Config:
@@ -15,23 +17,36 @@ class Config:
     step_function_orchestration_arn: str
 
     def __init__(self) -> None:
+        if os.path.exists("samconfig.toml"):
+            with open("samconfig.toml", mode="r") as f:
+                config = toml.load(f)
+                self.stack_name = config["default"]["global"]["parameters"][
+                    "stack_name"
+                ]
+                parameter_overrides = config["default"]["deploy"]["parameters"][
+                    "parameter_overrides"
+                ]
 
-        with open("samconfig.toml", mode="r") as f:
-            config = toml.load(f)
-            self.stack_name = config['default']['global']['parameters']['stack_name']
-            for entry in config['default']['deploy']['parameters']['parameter_overrides'].split(" "):
-                key, value = entry.split("=")
-                if key == 'PartnerIdParameter':
-                    self.spmid = value.replace("\"", "")
-                if key == 'StageParameter':
-                    self.stage = value.replace("\"", "")
-                if key == "WorkflowNotificationEmail":
-                    self.email = value.replace("\"", "")
-        for step_function in sfn.list_state_machines()['stateMachines']:
-            if step_function['name'] == f"AIF-Partner-to-AWS-Orchestrate-{self.stage}":
-                self.step_function_orchestration_arn = step_function['stateMachineArn']
-            if step_function['name'] == f"AIF-Partner-to-AWS-{self.stage}]":
-                self.step_function_arn = step_function['stateMachineArn']
+        else:
+            self.stack_name = os.environ["STACK_NAME"]
+            parameter_overrides = os.environ["PARAMETER_OVERRIDES"]
+
+        for entry in parameter_overrides.split(" "):
+            key, value = entry.split("=")
+            if key == "PartnerIdParameter":
+                self.spmid = value.replace('"', "")
+            elif key == "StageParameter":
+                self.stage = value.replace('"', "")
+            elif key == "WorkflowNotificationEmail":
+                self.email = value.replace('"', "")
+            else:
+                raise ValueError(f"Unknown parameter: {key}")
+
+        for step_function in sfn.list_state_machines()["stateMachines"]:
+            if step_function["name"] == f"AIF-Partner-to-AWS-Orchestrate-{self.stage}":
+                self.step_function_orchestration_arn = step_function["stateMachineArn"]
+            if step_function["name"] == f"AIF-Partner-to-AWS-{self.stage}]":
+                self.step_function_arn = step_function["stateMachineArn"]
         self._api_gateway_url = None
 
     @property
@@ -43,6 +58,9 @@ class Config:
         if not self._api_gateway_url:
             stack = cloudformation.Stack(self.stack_name)
             outputs = stack.outputs
-            self._api_gateway_url = [output['OutputValue']
-                                     for output in outputs if output['OutputKey'] == 'ApiGatewayURL'][0]
+            self._api_gateway_url = [
+                output["OutputValue"]
+                for output in outputs
+                if output["OutputKey"] == "ApiGatewayURL"
+            ][0]
         return self._api_gateway_url
