8ef06a49638dbdc0f2d10c72392e0499c50a3f34
Timo Runge <timorunge@users.noreply.github.com>
2023-06-12 10:31:07 +0200

    shellcheck and pylint samples. (#3)

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,15 +1,16 @@
-from datetime import datetime
-from dataclasses import dataclass
-import os
-import pytest
+"""
+Provides Configuration test functions.
+"""
 
+import os
+from datetime import datetime
+import pytest
 
 @pytest.fixture
 def execution_name():
+    """Returns the name of the test in the format {TESTNAME_DATE}"""
     str_date = datetime.now().strftime("%Y%d%m%H%M%S")
     test_name = os.environ.get(
         'PYTEST_CURRENT_TEST').split(':')[-1].split(' ')[0]
-
     name = f"{test_name}_{str_date}"
-
     return name
